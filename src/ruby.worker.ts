7977b256f6a08ddf29f80e504553fb68a0d129bc
Yuta Saito <kateinoigakukun@gmail.com>
2023-12-20 14:04:53 +0000

    Remove workaround for browser_wasi_shim

--- src/ruby.worker.ts
+++ src/ruby.worker.ts
@@ -1,4 +1,4 @@
-import { Directory, File, OpenFile, PreopenDirectory, SyncOPFSFile, WASI, strace } from "@bjorn3/browser_wasi_shim"
+import { Directory, File, OpenFile, PreopenDirectory, SyncOPFSFile, WASI } from "@bjorn3/browser_wasi_shim"
 import * as Comlink from "comlink"
 import { IFs, RubyInstall } from "./ruby-install"
 
@@ -201,25 +201,6 @@ export class RubyWorker {
         }
         const printer = consolePrinter((fd, str) => { log(str) })
         printer.addToImports(imports)
-
-        {
-            // WORKAROUND: browser_wasi_shim does not support some syscalls yet
-            // and returns -1 instead of proper ERRNO values and it results in confusing
-            // error messages "Success -- /path/to/file".
-            // Update browser_wasi_shim version when my fix[^1] will be released.
-            // [^1]: https://github.com/bjorn3/browser_wasi_shim/commit/6193f7482633ef818604375d9755ded67946adfc
-            const syscalls = imports["wasi_snapshot_preview1"]
-            for (const name of Object.keys(syscalls)) {
-                const original = syscalls[name];
-                syscalls[name] = (...args: any[]) => {
-                    const result = original(...args);
-                    if (result === -1) {
-                        return 58; // ENOTSUP
-                    }
-                    return result;
-                };
-            }
-        }
 
         const instnace: any = await WebAssembly.instantiate(this.module, imports);
         printer.setMemory(instnace.exports.memory);
